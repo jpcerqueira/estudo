015731146050abe5a3a501302851f797b4e1bf46
João Pedro Cerqueira <eng.joao@live.com>
2024-12-04 20:12:11 -0300

    desafio 1 do curso de lógica da programação da DIO

---
+++ DIO/teste.js
@@ -0,0 +1,11 @@
+//saída
+console.log('Hello World')
+//declarando variavel
+let nickname = 'Felipe'
+//saída + concantenar varialvel
+console.log('Bem vindo ' + nickname)
+console.log(nickname + ' entrou no servidor.')
+//declarando constante
+const notificacao = 'Pokemon Go diz:'
+//saída + concantenar constante
+console.log(notificacao + ' tem um novo pokemon na região.')
